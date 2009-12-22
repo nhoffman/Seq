e0fd3a8740c5f5b0a25a31486d8b0f22978457cb
nhoffman <nhoffman@a5eaa85c-ed6d-4a94-a6a1-3dbafb5ab095>
2009-12-22 06:17:56 +0000

    - moved functions from findDuplicates.py into sequtil.py
    git-svn-id: https://web.labmed.washington.edu/svn_pub/Seq/trunk@3267 a5eaa85c-ed6d-4a94-a6a1-3dbafb5ab095

--- scripts/findDuplicates.py
+++ scripts/findDuplicates.py
@@ -2,23 +2,12 @@
 
 import sys
 import logging
-import collections
 from optparse import OptionParser
 import pprint
 import time
-from itertools import izip, chain, repeat, islice, takewhile
-import cProfile
-import pstats
-import operator
 
 import Seq
-
-try:
-    import pp
-except ImportError:
-    pp_ok = False
-else:
-    pp_ok = True
+from Seq.sequtil import split_and_merge
 
 log = logging
 
@@ -29,167 +18,6 @@ log = logging
 #     pass
 
 # izip_longest is new in Python 2.6, provide replacement for Python 2.5
-try:
-    from itertools import izip_longest
-except ImportError:
-    def izip_longest(*args, **kwds):
-        # see http://docs.python.org/library/itertools.html#recipes
-        # izip_longest('ABCD', 'xy', fillvalue='-') --> Ax By C- D-
-        fillvalue = kwds.get('fillvalue')
-        def sentinel(counter = ([fillvalue]*(len(args)-1)).pop):
-            yield counter()         # yields the fillvalue, or raises IndexError
-        fillers = repeat(fillvalue)
-        iters = [chain(it, sentinel(), fillers) for it in args]
-        try:
-            for tup in izip(*iters):
-                yield tup
-        except IndexError:
-            pass
-
-def grouper(n, iterable, pad=True):
-    """
-    Return sequence of n-tuples composed of successive elements
-    of iterable; last tuple is padded with None if necessary. Not safe
-    for iterables with None elements.
-    """
-
-    args = [iter(iterable)] * n
-    iterout = izip_longest(fillvalue=None, *args)
-
-    if pad:
-        return iterout
-    else:
-        return (takewhile(lambda x: x is not None, c) for c in iterout)
-
-def flatten(d):
-    return sorted(d.keys() + list(chain(*d.values())))
-
-#### the real work is done in coalesce and merge
-
-def coalesce(strings, idx=None, comp='contains', log=log):
-
-    """
-    Groups a collection of strings by identifying the longest string
-    representing each nested set of substrings (if comp='contains') or
-    into sets of identical strings (if comp='eq')
-
-    Input
-    =====
-
-     * strings - a tuple of N strings
-     * idx - an optional sequence of integer indices into strings; if missing,
-       will include all elements in strings.
-
-     Output
-     ======
-
-     * a dict keyed by indices in strings. Each key i returns a list of
-       indices corresponding to strings nested within (or identical to) the
-       string at strings[i].
-    """
-
-    start = time.time()
-
-    if idx:
-        idx = list(idx)
-    else:
-        idx = range(len(strings))
-
-    if __debug__:
-        idx_orig = idx[:]
-
-    # sort idx by length, descending
-    idx.sort(key=lambda i: len(strings[i]),reverse=True)
-    log.debug('sort completed at %s secs' % (time.time()-start))
-    nstrings = len(idx)
-
-    #d = collections.defaultdict(list, ((i,list()) for i in idx))
-    d = dict((i,list()) for i in idx)
-
-    # operator.eq(a,b) <==> a == b
-    # operator.contains(a,b) <==> b in a
-    compfun = getattr(operator, comp)
-
-    cycle = 0
-    while len(idx) > 0:
-        parent_i = idx.pop(0)
-        parent_str = strings[parent_i]
-        if __debug__: # suppress using python -O
-            cycle += 1
-            log.debug('cycle %3s, %3s remaining, parent length=%4s' % \
-                (cycle, len(idx), len(strings[parent_i])))
-
-        for i, child_i in enumerate(idx):
-            # if strings[child_i] in parent_str:
-            if compfun(parent_str,strings[child_i]):
-                # assert child_i == idx.pop(i)
-                d[parent_i].append(idx.pop(i))
-                del d[child_i]
-
-        if __debug__:
-            log.debug("i=%-5s len(parent)=%3s children:%s" % \
-                (parent_i, len(strings[parent_i]), d.get(parent_i, 'no children')))
-
-    log.warning('Coalesce %s strings to %s in %.2f secs' % (nstrings, len(d), time.time()-start))
-
-    if __debug__:
-        dFlat = flatten(d)
-        log.info('checking d of length %s with min,max=%s,%s' % \
-            (len(d), min(dFlat), max(dFlat)))
-
-        assert set(idx_orig) == set(dFlat)
-
-        for parent, children in d.items():
-            for child in children:
-                assert strings[child] in strings[parent]
-
-    return d
-
-def merge(strings, d1, d2=None, comp='contains'):
-
-    """
-    Merge two dictionaries mapping superstrings to substrings.
-
-    Input
-    =====
-
-     * strings - a tuple of N strings
-     * d1, d2 - output of coalesce()
-     * comp - type of string comparison, passed to coalesce ("contains" or "eq")
-
-    Output
-    ======
-
-     * a single dict mapping superstrings to substrings
-
-    """
-
-    if d2 is None:
-        log.warning('d2 not provided, returning d1')
-        return d1
-
-    d = coalesce(strings, idx=d1.keys()+d2.keys(), comp=comp)
-
-    for i, dvals in d.items():
-        if dvals:
-            d[i].extend(list(chain(*[d1.get(j,[]) for j in dvals])))
-            d[i].extend(list(chain(*[d2.get(j,[]) for j in dvals])))
-        d[i].extend(d1.get(i,[]))
-        d[i].extend(d2.get(i,[]))
-
-    if __debug__:
-        d1Flat, d2Flat, dFlat = flatten(d1), flatten(d2), flatten(d)
-        log.info('checking d of length %s with min,max=%s,%s' % \
-            (len(d),min(dFlat),max(dFlat)))
-
-        assert set(d1Flat + d2Flat) == set(dFlat)
-
-        for parent, children in d.items():
-            for child in children:
-                assert strings[child] in strings[parent]
-
-
-    return d
 
 def rlist(d, seqs, fout):
 
@@ -288,23 +116,9 @@ findDuplicates -h prints options"""
 
     log.warning('chunksize = %s' % chunksize)
 
-    ### the important stuff happens from here...
-    chunks = grouper(n=chunksize, iterable=xrange(nstrings), pad=False)
-    # TODO: parallelize me
-    coalesced = [coalesce(strings, c, comp=comp) for c in chunks]
-
-    cycle = 1
-    while len(coalesced) > 1:
-        log.warning('merge cycle %s, %s chunks' % (cycle,len(coalesced)))
-        # TODO: parallelize me
-        coalesced = [merge(strings, d1, d2, comp=comp) for d1,d2 in grouper(n=2, iterable=coalesced)]
-        cycle += 1
-
-    d = coalesced[0]
-    ### ... to here
-
-    assert set(flatten(d)) == set(range(nstrings))
-
+    # d = split_and_merge(strings, comp, chunksize)
+    # d = split_and_merge(strings)
+    
     log.warning('Input data can be represented by %s superstrings (%.2f%% of the input number)' % (len(d), 100*(len(d)/float(nstrings))))
     log.warning('grand total is %.2f secs' % (time.time()-start))
 
--- sequtil.py
+++ sequtil.py
@@ -6,6 +6,36 @@ import Seq
 import os
 import logging
 
+import time
+import operator
+
+try:
+    import pp
+except ImportError:
+    pp_ok = False
+else:
+    pp_ok = True
+
+from itertools import izip, chain, repeat, islice, takewhile
+    
+try:
+    from itertools import izip_longest
+except ImportError:
+    def izip_longest(*args, **kwds):
+        # see http://docs.python.org/library/itertools.html#recipes
+        # izip_longest('ABCD', 'xy', fillvalue='-') --> Ax By C- D-
+        fillvalue = kwds.get('fillvalue')
+        def sentinel(counter = ([fillvalue]*(len(args)-1)).pop):
+            yield counter()         # yields the fillvalue, or raises IndexError
+        fillers = repeat(fillvalue)
+        iters = [chain(it, sentinel(), fillers) for it in args]
+        try:
+            for tup in izip(*iters):
+                yield tup
+        except IndexError:
+            pass
+
+
 log = logging
 
 from Dictionaries import translationWithoutAmbiguity, translationWithAmbiguity, translationWithoutAmbiguity3, translationWithAmbiguity3, complementDict, threeToOneLetterAADict, IUPAC_rev
@@ -646,25 +676,197 @@ def run_bat(cmds, name=None, path='.', run=True, cleanup=True):
         os.remove(outfilename)
 
 
-def test():
-    import sys, pprint, os
-    from io_clustal import readAlnStr
+######## functions used in findDuplicates.py
+def grouper(n, iterable, pad=True):
+    """
+    Return sequence of n-tuples composed of successive elements
+    of iterable; last tuple is padded with None if necessary. Not safe
+    for iterables with None elements.
+    """
 
-    #mdir,_ = os.path.split(__file__)
-    #infile = os.path.join(mdir, 'testfiles', 'oscillo_F41669_top20.aln')
-    infile = os.path.join('testfiles', 'oscillo_F41669_top20.aln')
+    args = [iter(iterable)] * n
+    iterout = izip_longest(fillvalue=None, *args)
 
-    seqlist = readAlnStr(open(infile).read())
-    seq = seqlist[0]
+    if pad:
+        return iterout
+    else:
+        return (takewhile(lambda x: x is not None, c) for c in iterout)
 
-    print wrap(seq)
+def flatten(d):
+    return sorted(d.keys() + list(chain(*d.values())))
 
-    for block in reformat_alignment(seqlist):
-        for line in block:
-            print line
-        print
+#### the real work is done in coalesce and merge
 
-    for block in reformat_alignment(seqlist, exclude_invariant=True):
-        for line in block:
-            print line
-        print
+def coalesce(strings, idx=None, comp='contains', log=log):
+
+    """
+    Groups a collection of strings by identifying the longest string
+    representing each nested set of substrings (if comp='contains') or
+    into sets of identical strings (if comp='eq')
+
+    Input
+    =====
+
+     * strings - a tuple of N strings
+     * idx - an optional sequence of integer indices into strings; if missing,
+       will include all elements in strings.
+
+     Output
+     ======
+
+     * a dict keyed by indices in strings. Each key i returns a list of
+       indices corresponding to strings nested within (or identical to) the
+       string at strings[i].
+    """
+
+    start = time.time()
+
+    if idx:
+        idx = list(idx)
+    else:
+        idx = range(len(strings))
+
+    if __debug__:
+        idx_orig = idx[:]
+
+    # sort idx by length, descending
+    idx.sort(key=lambda i: len(strings[i]),reverse=True)
+    log.debug('sort completed at %s secs' % (time.time()-start))
+    nstrings = len(idx)
+
+    #d = collections.defaultdict(list, ((i,list()) for i in idx))
+    d = dict((i,list()) for i in idx)
+
+    # operator.eq(a,b) <==> a == b
+    # operator.contains(a,b) <==> b in a
+    compfun = getattr(operator, comp)
+
+    cycle = 0
+    while len(idx) > 0:
+        parent_i = idx.pop(0)
+        parent_str = strings[parent_i]
+        if __debug__: # suppress using python -O
+            cycle += 1
+            log.debug('cycle %3s, %3s remaining, parent length=%4s' % \
+                (cycle, len(idx), len(strings[parent_i])))
+
+        for i, child_i in enumerate(idx):
+            # if strings[child_i] in parent_str:
+            if compfun(parent_str,strings[child_i]):
+                # assert child_i == idx.pop(i)
+                d[parent_i].append(idx.pop(i))
+                del d[child_i]
+
+        if __debug__:
+            log.debug("i=%-5s len(parent)=%3s children:%s" % \
+                (parent_i, len(strings[parent_i]), d.get(parent_i, 'no children')))
+
+    log.warning('Coalesce %s strings to %s in %.2f secs' % (nstrings, len(d), time.time()-start))
+
+    if __debug__:
+        dFlat = flatten(d)
+        log.info('checking d of length %s with min,max=%s,%s' % \
+            (len(d), min(dFlat), max(dFlat)))
+
+        assert set(idx_orig) == set(dFlat)
+
+        for parent, children in d.items():
+            for child in children:
+                assert strings[child] in strings[parent]
+
+    return d
+
+def merge(strings, d1, d2=None, comp='contains'):
+
+    """
+    Merge two dictionaries mapping superstrings to substrings.
+
+    Input
+    =====
+
+     * strings - a tuple of N strings
+     * d1, d2 - output of coalesce()
+     * comp - type of string comparison, passed to coalesce ("contains" or "eq")
+
+    Output
+    ======
+
+     * a single dict mapping superstrings to substrings
+
+    """
+
+    if d2 is None:
+        log.warning('d2 not provided, returning d1')
+        return d1
+
+    d = coalesce(strings, idx=d1.keys()+d2.keys(), comp=comp)
+
+    for i, dvals in d.items():
+        if dvals:
+            d[i].extend(list(chain(*[d1.get(j,[]) for j in dvals])))
+            d[i].extend(list(chain(*[d2.get(j,[]) for j in dvals])))
+        d[i].extend(d1.get(i,[]))
+        d[i].extend(d2.get(i,[]))
+
+    if __debug__:
+        d1Flat, d2Flat, dFlat = flatten(d1), flatten(d2), flatten(d)
+        log.info('checking d of length %s with min,max=%s,%s' % \
+            (len(d),min(dFlat),max(dFlat)))
+
+        assert set(d1Flat + d2Flat) == set(dFlat)
+
+        for parent, children in d.items():
+            for child in children:
+                assert strings[child] in strings[parent]
+
+
+    return d
+        
+def split_and_merge(strings, comp='contains', chunksize=None):
+
+    """
+    Given a sequence of strings, return a dictionary mapping
+    superstrings to substrings.
+
+    Input
+    =====
+
+     * strings - a tuple of N strings
+     * comp - defines string comparison method: 'contains' -> "s1 in s2" or
+       'eq' -> "s1 == s2"
+     * chunksize - an integer defining size of partitions into which
+       strings are divided; each partition is coalesced individually, and the
+       results of each are merged.
+
+    Output
+    ======
+
+     * A dict mapping superrstrings to substrings, in which keys and
+     values are indices in strings.
+       
+    """
+    
+    nstrings = len(strings)
+    
+    if not chunksize:
+        chunksize = nstrings
+
+    ### the important stuff happens from here...
+    chunks = grouper(n=chunksize, iterable=xrange(nstrings), pad=False)
+    # TODO: parallelize me
+    coalesced = [coalesce(strings, c, comp=comp) for c in chunks]
+
+    cycle = 1
+    while len(coalesced) > 1:
+        log.warning('merge cycle %s, %s chunks' % (cycle,len(coalesced)))
+        # TODO: parallelize me
+        coalesced = [merge(strings, d1, d2, comp=comp) for d1,d2 in grouper(n=2, iterable=coalesced)]
+        cycle += 1
+
+    d = coalesced[0]
+    ### ... to here
+
+    assert set(flatten(d)) == set(range(nstrings))
+    
+    return d
+    
